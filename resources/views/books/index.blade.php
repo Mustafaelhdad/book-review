10f8460f7aaaafa99e39254f6e2c0fcac6758c54
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2025-01-26 01:23:48 +0200

    filtering by the book title

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,38 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mb-10 text-2xl">Books</h1>
+		<h1 class="mb-10 text-2xl">Books</h1>
 
-    <form action=""></form>
+		<form class="mb-4 flex items-center space-x-2" method="GET" action="{{ route('books.index') }}">
+				<input class="input h-10" name="title" type="text" value="{{ request('title') }}" placeholder="Search">
+				<button class="btn h-10" type="submit">Search</button>
+				<a class="btn h-10" href="{{ route('books.index') }}">Clear</a>
+		</form>
 
-    <ul>
-      @forelse ($books as $book)
-        <li class="mb-4">
-          <div class="book-item">
-            <div
-            class="flex flex-wrap items-center justify-between">
-              <div class="w-full flex-grow sm:w-auto">
-                <a href="{{route('books.show', $book)}}" class="book-title">{{$book->title}}</a>
-                <span class="book-author">by {{$book->author}}</span>
-              </div>
-              <div>
-                <div class="book-rating">
-                  {{ round($book->reviews_avg_rating, 1) ?? 'No ratings yet' }}
-                </div>
-                <div class="book-review-count">
-                  out of {{ $book->reviews_count ?? 0 }} {{Str::plural('review', $book->reviews_count)}}
-                </div>
-              </div>
-            </div>
-            </div>
-        </li>
-      @empty
-        <li class="mb-4">
-          <div class="empty-book-item">
-            <p class="empty-text text-lg font-semibold">No reviews yet</p>
-            <a href="{{route('books.index')}}" class="reset-link">Reset criteria</a>
-          </div>
-        </li>
-      @endforelse
-    </ul>
+		<ul>
+				@forelse ($books as $book)
+						<li class="mb-4">
+								<div class="book-item">
+										<div class="flex flex-wrap items-center justify-between">
+												<div class="w-full flex-grow sm:w-auto">
+														<a class="book-title" href="{{ route('books.show', $book) }}">{{ $book->title }}</a>
+														<span class="book-author">by {{ $book->author }}</span>
+												</div>
+												<div>
+														<div class="book-rating">
+																{{ round($book->reviews_avg_rating, 1) ?? 'No ratings yet' }}
+														</div>
+														<div class="book-review-count">
+																out of {{ $book->reviews_count ?? 0 }} {{ Str::plural('review', $book->reviews_count) }}
+														</div>
+												</div>
+										</div>
+								</div>
+						</li>
+				@empty
+						<li class="mb-4">
+								<div class="empty-book-item">
+										<p class="empty-text text-lg font-semibold">No reviews yet</p>
+										<a class="reset-link" href="{{ route('books.index') }}">Reset criteria</a>
+								</div>
+						</li>
+				@endforelse
+		</ul>
 @endsection
